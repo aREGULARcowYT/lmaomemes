e035905c289ace07721b9cd1047cd20f3838f78e
aREGULARcowYT <114805854+aREGULARcowYT@users.noreply.github.com>
2022-10-02 17:41:42 +0530

    Add files via upload

---
+++ colorChange.js
@@ -0,0 +1,10 @@
+const memeBtn=document.getElementById('memeBtn')
+
+const bg=[" #ff3381" , " #93f" , " #f56" , " #eeff00" , " 003cff"]
+const fg=[ " #111" , " #fff" , "#111" , " #111" , " #fff"]
+
+
+function colorChange () {
+    
+
+}
